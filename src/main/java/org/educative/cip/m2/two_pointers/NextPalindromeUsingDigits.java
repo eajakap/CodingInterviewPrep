bd4b25b76310e688bffbd768980a73859f5e45d0
Ajay Kapoor <ajaykapoor40@gmail.com>
2026-09-06 15:59:53 -0400

    Examples to support Two Pointers

--- src/main/java/org/educative/cip/m2/two_pointers/NextPalindromeUsingDigits.java
+++ src/main/java/org/educative/cip/m2/two_pointers/NextPalindromeUsingDigits.java
@@ -2,8 +2,30 @@ package org.educative.cip.m2.two_pointers;
 import java.util.*;
 
 /*
-    * Time Complexity: O(n) - We traverse the digits to find the next permutation and then construct the palindrome.
-    * Space Complexity: O(n) - We use a list to store the left half of the digits.
+ * Problem: Given a numeric string, numStr, representing a palindrome (composed only of digits).
+ * Return the smallest palindrome larger than numStr that can be created by rearranging its digits.
+ * If no such palindrome exists, return an empty string "".
+ * Consider the following example to understand the expected output for a given numeric string:
+ * input string = "123321"
+ * The valid palindromes made from the exact digits are "213312", "231132", "312213", "132231", "321123".
+ * We return the palindrome "132231" because it is the smallest palindrome larger than the input string "123321".
+ * Steps:
+ * 1. Split the palindrome into two halves (left and right).
+ *    For even-length palindromes, divide the string into two equal halves.
+ *    For odd-length palindromes, preserve the middle digit and only modify the left half.
+ * 2. Find the next permutation of the left half.
+ *    - Traverse the left half from right to left to find the first pair of digits where the left digit is smaller than the right digit.
+ *      - Start from the end of leftHalf and move left to find the first index i where digits[i] < digits[i + 1].
+ *      - Then, scan again from the end to find index j where digits[j] > digits[i].
+ *      - If such a pair exists, it indicates that a next permutation is possible.
+ *      - If no such pair exists, the left half is in descending order, and no next permutation exists.
+ *    - Swap this left digit with the smallest digit on its right that is larger than it.
+ *    - Reverse the digits to the right of the original left digit's position to get the next permutation.
+ * 3. If a next permutation exists, construct the new palindrome by mirroring the left half and adding the middle character (if the length is odd).
+ * 4. If no next permutation exists, return "-1" indicating that no greater palindrome can be formed.
+ *
+ * Time Complexity: O(n) - We traverse the digits to find the next permutation and then construct the palindrome.
+ * Space Complexity: O(n) - We use a list to store the left half of the digits.
  */
 public class NextPalindromeUsingDigits {
 
@@ -68,6 +90,7 @@ public class NextPalindromeUsingDigits {
                 return false;
             }
             // Find the largest index j greater than i such that digits[j] > digits[i].
+            // ( First index from the end where the digit is greater than digits[i])
             int j = digits.size() - 1;
             while (digits.get(j) <= digits.get(i)) {
                 j--;
@@ -118,7 +141,7 @@ public class NextPalindromeUsingDigits {
         }
 
         public static void main(String[] args) {
-            String[] testCases = {"1221", "54345", "999", "12321", "89798"};
+            String[] testCases = {"454213456","875137623", "1221", "2354532", "54345", "999", "12321", "89798"};
 
             for (int i = 0; i < testCases.length; i++) {
                 System.out.println(i + 1 + ".\t Original palindrome: '" + testCases[i] + "'");
