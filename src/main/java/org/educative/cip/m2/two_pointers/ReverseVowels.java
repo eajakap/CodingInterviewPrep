d229c13f5ca46c22fbbdb9d1ebce7791af0e5e5d
Ajay Kapoor <ajaykapoor40@gmail.com>
2026-09-08 08:53:31 -0400

    Examples to support Two Pointers

--- src/main/java/org/educative/cip/m2/two_pointers/ReverseVowels.java
+++ src/main/java/org/educative/cip/m2/two_pointers/ReverseVowels.java
@@ -4,12 +4,42 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * Problem: Reverse Vowels of a String
+ * Given a string s, reverse only all the vowels in the string and return it.
+ * Example 1:
+ * Input: s = "hello"
+ * Output: "holle"
+ * Example 2:
+ * Input: s = "leetcode"
+ * Output: "leotcede"
+ * Constraints:
+ * 1 <= s.length <= 3 * 10^5
+ * s consist of printable ASCII characters.
+ *
+ *  Steps to solve the problem:
+ *  1. Create a set of vowels for quick lookup.
+ *  2. Convert the string to a character array for in-place modification.
+ *  3. Use two pointers, one starting from the beginning (left) and the other from the end (right) of the character array.
+ *  4. Move the left pointer to the right until it points to a vowel, and move the right pointer to the left until it points to a vowel.
+ *  5. Swap the vowels at the left and right pointers
+ *  6. Move both pointers towards the center and repeat steps 4-5 until the pointers meet or cross.
+ *  7. Convert the modified character array back to a string and return it.
+ *
  * Time Complexity: O(n), where n is the length of string s. We traverse through string s once.
  * Space Complexity: O(1), as we are using a constant amount of space for the set of vowels.
  * Given a string s, reverse only all the vowels in the string and return it.
  */
 public class ReverseVowels {
 
+    // Function to reverse the vowels in a given string
+    // Steps:
+    //  1. Create a set of vowels for quick lookup.
+    //  2. Convert the string to a character array for in-place modification.
+    //  3. Use two pointers, one starting from the beginning (left) and the other from the end (right) of the character array.
+    //  4. Move the left pointer to the right until it points to a vowel, and move the right pointer to the left until it points to a vowel.
+    //  5. Swap the vowels at the left and right pointers
+    //  6. Move both pointers towards the center and repeat steps 4-5 until the pointers meet or cross.
+    //  7. Convert the modified character array back to a string and return it.
     public String reverseVowels(String s) {
         Set<Character> vowels = new HashSet<>();
         for (char c : "aeiouAEIOU".toCharArray()) {
