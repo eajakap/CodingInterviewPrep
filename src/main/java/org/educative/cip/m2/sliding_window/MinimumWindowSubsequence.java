fe188470b0eb09d54982add042d8041dc15b1820
Ajay Kapoor <ajaykapoor40@gmail.com>
2026-09-09 09:05:34 -0400

    Examples to support Two Pointers

--- src/main/java/org/educative/cip/m2/sliding_window/MinimumWindowSubsequence.java
+++ src/main/java/org/educative/cip/m2/sliding_window/MinimumWindowSubsequence.java
@@ -30,41 +30,71 @@ package org.educative.cip.m2.sliding_window;
  */
 public class MinimumWindowSubsequence {
 
+    /**
+     * Returns the minimum window in str1 which will contain all the characters of str2 in order.
+     * If there is no such window in str1 that covers all characters of str2, returns the empty string "".
+     * If there are multiple minimum windows, returns the one with the left-most starting index.
+     *
+     * @param str1 The string to search within.
+     * @param str2 The string containing the characters to find in order.
+     * @return The minimum window subsequence of str1 that contains all characters of str2 in order.
+     */
     public static String minWindow(String str1, String str2) {
         // Save the size of str1 and str2
         int sizeStr1 = str1.length();
         int sizeStr2 = str2.length();
         // Initialize the minimum length to positive infinity and indices for str1 and str2
-        float length = Float.POSITIVE_INFINITY;
+        float minSubLen = Float.POSITIVE_INFINITY;
         // Initialize indices for str1 and str2
         int indexS1 = 0;
         int indexS2 = 0;
         int start = 0, end = 0;
         String minSubsequence = "";
+        // Process every character of str1
         while (indexS1 < sizeStr1) {
+            // Check if the character pointed by indexS1 in str1
+            // is the same as the character pointed by indexS2 in
             if (str1.charAt(indexS1) == str2.charAt(indexS2)) {
-                indexS2 += 1; // increment indexS2 to check for next character in str2
+                // If the pointed character is the same
+                // in both strings increment indexS2
+                indexS2 += 1;
+                // Check if indexS2 has reached the end of str2
                 if (indexS2 == sizeStr2) {
+                    // At this point the str1 contains all characters of str2
                     start = indexS1;
                     end = indexS1;
-                    indexS2 -= 1; // decrement indexS2 to check for last character in str2
+                    // Initialize start to the index where all characters of
+                    // str2 were present in str1
+                    indexS2 -= 1;
+                    // Decrement pointer indexS2 and start a reverse loop
                     while (indexS2 >= 0) {
+                        // Decrement pointer indexS2 until all characters of
+                        // str2 are found in str1
                         if (str1.charAt(start) == str2.charAt(indexS2)) {
                             indexS2 -= 1;
                         }
+                        // Decrement start pointer everytime to find the
+                        // starting point of the required subsequence
                         start -= 1;
                     }
                     // increment start to point to the first character of the subsequence
                     start += 1;
-                    // check if the length of the current subsequence is less than the minimum length found so far
-                    if ((end - start + 1) < length) {
-                        length = end - start + 1;
+                    // Check if length of sub sequence
+                    // by start and end pointers is less than current min length
+                    if ((end - start + 1) < minSubLen) {
+                        // Update minSubLen if current sub sequence is
+                        minSubLen = end - start + 1;
+                        // Update minimum subsequence string
+                        // to this new shorter string
                         minSubsequence = str1.substring(start, end + 1);
                     }
+                    // Set indexS1 to start to continue checking in str1
+                    // after this discovered subsequence
                     indexS1 = start;
                     indexS2 = 0;
                 }
             }
+            // Increment pointer indexS1 to check next character in str1
             indexS1 += 1;
         }
         return minSubsequence;
