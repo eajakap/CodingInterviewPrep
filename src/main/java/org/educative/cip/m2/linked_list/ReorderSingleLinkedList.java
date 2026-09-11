6dd4f78d1c518204365ed83ba54487c8c7cc783e
Ajay Kapoor <ajaykapoor40@gmail.com>
2026-09-11 00:32:04 -0400

    Examples to support Two Pointers

--- src/main/java/org/educative/cip/m2/linked_list/ReorderSingleLinkedList.java
+++ src/main/java/org/educative/cip/m2/linked_list/ReorderSingleLinkedList.java
@@ -93,8 +93,44 @@ public class ReorderSingleLinkedList {
         }
     }
 
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static ListNode mergeLists(ListNode firstHead, ListNode secondHead) {
+        ListNode first = firstHead;
+        ListNode second = secondHead;
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+        return firstHead;
+    }
+
     // Function to reorder the linked list
-    private static void reorderList(ListNode head) {
+    public static void reorderList(ListNode head) {
         if (head == null || head.next == null) return;
 
         // Find the middle of the list
